728625cd376d64278666cf0b1c2cf9681b3b0132
Hizbucodes <hizbusoftware@gmail.com>
2025-11-23 17:43:47 +0530

    add one-one comment and user relationship

--- StockMarket.API/Models/Comment.cs
+++ StockMarket.API/Models/Comment.cs
@@ -12,11 +12,11 @@ namespace StockMarket.API.Models
 
         public DateTime CreatedOn { get; set; } = DateTime.Now;
 
-
         public int? StockId { get; set; }
-
-
-
         public Stock? Stock { get; set; }
+
+
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
